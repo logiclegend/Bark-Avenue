80339ff744259f9b158001924ec06b1d00e39e66
Vitalii Rymarchuk <darkwarior4ek@gmail.com>
2024-03-12 00:39:39 +0200

    add some routing

--- ba-clients/bark-avenue/src/app/app.module.ts
+++ ba-clients/bark-avenue/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home-page/components/home/home.component';
 import { SiteHeaderComponent } from './Shared/components/site-header/site-header.component';
 import { SiteFooterComponent } from './Shared/components/site-footer/site-footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { EmptyPageComponent } from './pages/empty-page/empty-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SiteHeaderComponent,
-    SiteFooterComponent
+    SiteFooterComponent,
+    EmptyPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
