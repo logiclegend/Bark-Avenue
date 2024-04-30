e17ea86fc3e10f99c8ef4fdf8a94baa980ee38f0
Vitaliy-Rymarchuk-01 <161717192+Vitaliy-Rymarchuk-01@users.noreply.github.com>
2024-04-30 19:03:57 +0300

    integration completed

--- ba-clients/bark-avenue/src/app/user/registration/registration.component.ts
+++ ba-clients/bark-avenue/src/app/user/registration/registration.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ModalService } from 'src/app/Shared/services/modal.service';
 import { UserService } from '../user.service';
 import { IUserSignUpCredentials } from '../user.model';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-registration',
@@ -10,17 +12,28 @@ import { IUserSignUpCredentials } from '../user.model';
 })
 export class RegistrationComponent {
   credentials : IUserSignUpCredentials = {
-    name: '', 
+    username: '', 
     email: '',
-    number: '0977688807',
-    password: '',
+    phone_number: '0977688807',
+    password_user: '',
     confirm_password: ''
   }
 
-  constructor(protected modalService: ModalService , private userService: UserService) {}
+  constructor(protected modalService: ModalService , private userService: UserService , private router : Router) {}
 
   signUp(){
-    this.userService.signUp(this.credentials).subscribe();
+    this.userService.signUp(this.credentials).subscribe({
+      next: (res: any) => {
+        this.modalService.close();
+        this.router.navigate(['/home']);
+        setTimeout(() => {alert(res)}, 1500)
+      },
+      error: (err : HttpErrorResponse) => {
+        console.log(err.name);
+        console.log(err.message);
+        console.log(err.status);  
+      }
+  });
   }
 
 
